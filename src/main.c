524b20f990ea7aa9d39c0f4a779d767c32224102
OmarKHDR <omarsliman37@gmail.com>
2025-03-08 15:05:17 +0200

    the repl loop

--- src/main.c
+++ src/main.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  // Uncomment this block to pass the first stage
-  printf("$ ");
-
-  // Wait for user input
-  char input[100];
-  fgets(input, 100, stdin);
-  int end = strcspn(input, "\r\n");
-  input[end] = '\0';
-  printf("%s: command not found\n", input);
+  while (true) {
+    // Uncomment this block to pass the first stage
+    printf("$ ");
+    // Wait for user input
+    char input[100];
+    fgets(input, 100, stdin);
+    int end = strcspn(input, "\r\n");
+    input[end] = '\0';
+    printf("%s: command not found\n", input);
+  }
   return 0;
 }
